b949183b4c49be2a389c56a7fd118f9d24c0f3eb
Tamiris Figueiredo <ttamirisfds@gmail.com>
2023-09-14 19:31:17 -0300

    Criação da classe VetorOrdenado e método imprimir

---
+++ vetores-ordenados/vetor-ordenado.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class VetorOrdenado:
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.valores = np.empty(self.capacidade, dtype=int)
+    
+    # O(n)
+    def imprimir(self):
+        if self.ultima_posicao == -1:
+            print('O vetor está vazio')
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i, '-', self.valores[i])
