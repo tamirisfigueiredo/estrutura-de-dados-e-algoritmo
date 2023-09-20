6fe64f6da1a81d1fe3b8dd7f945d8d72d7189b6d
Tamiris Figueiredo <ttamirisfds@gmail.com>
2023-09-19 21:33:08 -0300

    Criação da classe FilaCircular

---
+++ pilhas-filas-deques/fila-circular.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class FilaCircular:
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.inicio = 0
+        self.final = -1
+        self.numero_elementos = 0
+        self.valores = np.empty(self.capacidade, dtype=int)
+    
+    
